c3ec86c847fc8a233afaf909cf3f6a83d4386c98
Lakshya Dev <lakshyadev@live.com>
2021-08-07 07:19:34 +0530

    updated onxx model generation logic

--- models/base_model.py
+++ models/base_model.py
@@ -207,23 +207,7 @@ class BaseModel(ABC):
                         state_dict, net, key.split('.'))
                 net.load_state_dict(state_dict)
 
-    def print_networks(self, verbose):
-        """Print the total number of parameters in the network and (if verbose) network architecture
-
-        Parameters:
-            verbose (bool) -- if verbose: print the network architecture
-        """
-        print('---------- Networks initialized -------------')
-        for name in self.model_names:
-            if isinstance(name, str):
-                net = getattr(self, 'net' + name)
-                num_params = 0
-                for param in net.parameters():
-                    num_params += param.numel()
-                if verbose:
-                    print(net)
-                print('[Network %s] Total number of parameters : %.3f M' %
-                      (name, num_params / 1e6))
+                # Generate onnx model
                 net.eval()
                 net.cuda()  # in this example using cuda()
 
@@ -246,6 +230,24 @@ class BaseModel(ABC):
 
                 print('The ONNX file ' + export_onnx_file +
                       ' is saved at %s' % save_path)
+
+    def print_networks(self, verbose):
+        """Print the total number of parameters in the network and (if verbose) network architecture
+
+        Parameters:
+            verbose (bool) -- if verbose: print the network architecture
+        """
+        print('---------- Networks initialized -------------')
+        for name in self.model_names:
+            if isinstance(name, str):
+                net = getattr(self, 'net' + name)
+                num_params = 0
+                for param in net.parameters():
+                    num_params += param.numel()
+                if verbose:
+                    print(net)
+                print('[Network %s] Total number of parameters : %.3f M' %
+                      (name, num_params / 1e6))
         print('-----------------------------------------------')
 
     def set_requires_grad(self, nets, requires_grad=False):
